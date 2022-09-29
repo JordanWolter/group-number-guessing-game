5d303a41e3e00127dca26e0016b26c95afe5dd98
Jordan Wolter <jordan.wolter@icloud.com>
2022-09-29 10:08:12 -0500

    Initial commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,10 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  $('#btn').on('submit', playerGuesses);
+}
+
+function playerGuesses(evt){
+  evt.preventDefault();
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+let guessList = [];
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -14,4 +16,21 @@ app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
+});
+
+app.get('/game', (req, res) => {
+  console.log('we want the numbers');
+
+  res.send(guessList);
+});
+
+app.post('/game', (req, res) => {
+  console.log('in game POST', req.body);
+
+  let newGuessFrom = req.body;
+
+  guessList.push(newGuessFrom);
+
+  res.sendStatus(201);
+
 })
